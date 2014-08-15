c6df8f45e313199d89f9b775e1c791ac521ab7fd
newbeezhang <zhanghaibin@xinmifeng.com.cn>
2014-08-15 11:09:45 +0800

    top里面登陆用户信息

--- sqlDb.php
+++ sqlDb.php
@@ -1,7 +1,7 @@
 <?php
 require ("./MysqliDb.php");
 //$DB = new Mysqlidb('192.168.1.106', 'root', 'Roberter@1234', 'iBus');
-$DB = new Mysqlidb('192.168.1.101', 'root', 'newbee', 'busfree');
+$DB = new Mysqlidb('192.168.1.101', 'newbee', 'newbee', 'busfree');
 if(!$DB) die("Database error");
 
 $prefix = 'bee_';
