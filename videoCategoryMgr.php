a8f8e904245db13293ae6fd7a0b890c11405658a
newbeeze <wangdaze@xinmifeng.com.cn>
2014-08-18 16:29:39 +0800

    类别

--- videoCategoryMgr.php
+++ videoCategoryMgr.php
@@ -16,8 +16,21 @@ $result = $DB->get("video_type");
 <!DOCTYPE html>
 <html>
 <meta charset="utf-8">
-	<head></head>
+	<head>
+	<script src="bower_components/jquery/jquery.js"></script>
+	<script src="js/Layerutility.js"></script>
+	</head>
 	<body>
+	<div style="display:none" id="DIV_Event">
+    <table>
+        <tr><td>视频分类名称：</td>
+        <td><input type="text" name="type_name" /></td>
+        </tr>
+		<tr><td>排序值</td>
+            <td><input type="text" name="order_id" /></td>
+        </tr>
+    </table>
+</div>
 		<table border="1">
 			<tr>
 				<td colspan="4" align="center">视频分类管理</td>
@@ -41,16 +54,21 @@ $result = $DB->get("video_type");
 			<? $i++; }?>
 			<tr>
 			<td colspan="4">
-				<input type="button" name="add" value="新增" onclick="to_addPage();" />
+				<input type="button" name="add" value="新增" onclick="javascript:to_addPage();" />
 				<input type="button" name="update" value="修改"/>
 				<input type="button" name="del" value="删除"/>
 			</td>
 			</tr>
 		</table>
-		<script type="javascript">
-		function to_addPage(){
-	   window.showModalDialog('addVideoCategory.php',window,'scroll:0;status:0;help:1;resizable:1;dialogWidth:480px;dialogHeight:320px');
-	   }
+		<script>
+		 //弹出添加页面
+        function to_addPage(){
+            var htm=$("#DIV_Event").html();
+            console.log(htm);
+            $.ShowHtmlByForm(htm,"添加视频分类");
+        }
+
+
 	   </script>
 	</body>
 </html>
