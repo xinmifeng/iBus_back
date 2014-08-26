7c95f11add4456b52175263b08c0f916ab2ad14b
newbeeze <wangdaze@xinmifeng.com.cn>
2014-08-26 11:03:02 +0800

    加

--- sqlDb.php
+++ sqlDb.php
@@ -1,6 +1,8 @@
 <?php
 //$DB = new Mysqlidb('192.168.1.106', 'root', 'Roberter@1234', 'iBus');
 $DB = new Mysqlidb('192.168.1.101', 'root', 'root', 'busfree');
+//$DB = new Mysqlidb('localhost', 'newbee', 'newbee', 'busfree');
+//$DB = new Mysqlidb('hdm-126.hichina.com', 'hdm1260830', '123qwe123', 'hdm1260830_db');
 if(!$DB) die("Database error");
 
 $prefix = 'bee_';
